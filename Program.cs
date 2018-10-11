6170b727d9833a28f256cda665ea5a8e1b156ec6
Josh Robinson <josh.robinson@gmail.com>
2018-10-11 19:58:39 -0400

    basic first version

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+namespace CognitiveOcr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imgUrl = "https://photos.app.goo.gl/aMacQbh9SeYnn6UF8";
+
+            Console.WriteLine("Please enter your api key");
+            var key = Console.ReadLine();
+
+
+        }
+    }
+}
