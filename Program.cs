fca2b4a45eccfe99c56836a4022d6a7900683d7c
Josh Robinson <josh.robinson@gmail.com>
2018-10-12 06:34:28 -0400

    the better part of the solution.. just a few details

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 namespace CognitiveOcr
 {
@@ -12,7 +14,30 @@ namespace CognitiveOcr
             Console.WriteLine("Please enter your api key");
             var key = Console.ReadLine();
 
+            var client = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(key),
+                new System.Net.Http.DelegatingHandler[]{}
+            );
 
+            client.Endpoint = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0";
+
+            Console.WriteLine("Analyzing...");
+            GetImageText(client, imgUrl).Wait();
+            Console.ReadKey();
+        }
+
+        static async Task GetImageText(ComputerVisionClient client, string imageUrl){
+
+            // Finally some AI calls
+            var ocrResult = await client.RecognizeTextAsync(imageUrl, TextRecognitionMode.Printed);
+            WriteResults(ocrResult);
+        }
+
+        private static void WriteResults(RecognizeTextHeaders result)
+        {
+            if (result != null){
+                foreach(var line in result.OperationLocation)
+            }
         }
     }
 }
