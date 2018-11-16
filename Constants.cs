d62d2ac4e7fb337ea50a4901564b987185a67713
Josh Robinson <josh.robinson@gmail.com>
2018-11-16 12:05:39 -0500

    finally some working code

---
+++ Constants.cs
@@ -0,0 +1,5 @@
+public static class Constants{
+    public const string API_KEY = ""; //put your api key here
+    public const string COGNITIVE_URL = "https://eastus.api.cognitive.microsoft.com/";
+    public const int OPERATION_LOCATION_ID_LENGTH = 36;
+}
--- Program.cs
+++ Program.cs
@@ -10,61 +10,43 @@ namespace CognitiveOcr
     {
         static void Main(string[] args)
         {
-            const string key = "000000"; //put a valid key here.
-
-            var client = new ComputerVisionClient(
-                new ApiKeyServiceClientCredentials(key),
-                new System.Net.Http.DelegatingHandler[] { }
-            );
-            client.Endpoint = "https://eastus.api.cognitive.microsoft.com/";
-
-
-         
+            var client = BuildClient();
 
             Console.WriteLine("Press any key to begin...");
-            
             Console.ReadKey();
 
             using(Stream imgStream = File.OpenRead("img\\words_img.jpg")){
                 Console.WriteLine("imgStream size " + imgStream.Length);
-                GetImageText(client, imgStream).Wait();
+                GetImageText(client, imgStream).Wait();                
             }
-            
+
             Console.WriteLine("Press any key to finish execution...");
             Console.ReadKey();
         }
 
         static async Task GetImageText(ComputerVisionClient client, Stream imgStream){
-
-            // Finally some AI calls
-            RecognizeTextInStreamHeaders ocrResult = 
-                await client.RecognizeTextInStreamAsync(imgStream, TextRecognitionMode.Printed);
             
-            await RetrieveAndWriteResults(client, ocrResult.OperationLocation);
+            OcrResult ocrResult = await client.RecognizePrintedTextInStreamAsync(false, imgStream, OcrLanguages.En);
+            foreach (var item in ocrResult.Regions)
+            {
+                foreach (var line in item.Lines)
+                {
+                    foreach(var word in line.Words)
+                        Console.WriteLine(word.Text);
+                }
+            }
         }
+        
+        public static ComputerVisionClient BuildClient() {
 
-        static async Task RetrieveAndWriteResults(ComputerVisionClient client, string operationLocation)
-        {
-            Console.WriteLine("Returned Location: " + operationLocation);
+            var client = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(Constants.API_KEY),
+                new System.Net.Http.DelegatingHandler[] { }
+            );
 
-            //lets get the Operation ID that is on the end of the operationLocation.
-            //we need this for our next call
-            string operationId = operationLocation.Substring(
-                operationLocation.Length - 36);
-            
-            Console.WriteLine("Operation ID: " + operationId);
+            client.Endpoint = Constants.COGNITIVE_URL;
 
-            TextOperationResult result =
-                await client.GetTextOperationResultAsync(operationId);
-            
-            while ((result.Status == TextOperationStatusCodes.Running ||
-                    result.Status == TextOperationStatusCodes.NotStarted)){
-                        Console.WriteLine("Client Status: " + result.Status);
-                        await Task.Delay(5000);
-                    }
-
-            foreach(Line line in result.RecognitionResult.Lines)
-                Console.WriteLine(line.Text);
+            return client;
         }
     }
 }
